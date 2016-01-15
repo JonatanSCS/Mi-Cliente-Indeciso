c1cdbfc51a6c3d8358455dcca56fc63fa575ebc3
JonatanSCS <jonatanscs97@hotmail.com>
2016-01-15 00:23:24 -0600

    type: source / Initializing project development files

---
+++ Gruntfile.js
@@ -0,0 +1,80 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    uglify: {
+      options: {
+        banner: 'Grunt file/\n'
+      },
+      build: {
+        //src: 'scripts/',
+        //dest: 'scripts/'
+      }
+    },
+    
+    jade: {
+      debug: {
+        options: {
+          data: {
+          debug: true
+          }
+        },
+        files: {
+          'index.html': 'index.jade',
+          
+        }
+      } 
+    },
+
+    stylus:{
+      debug: {
+        options: {
+          data: {
+          debug: true
+          }
+        },
+        files: {
+          'css/style.css': 'css/style.styl'
+        }
+      } 
+    },
+
+    connect: {
+      server: {
+        options: {
+          hostname: 'localhost',
+          port: 8000,
+          keepalive: true
+        }
+      }
+    },
+    watch: {
+            stylesheets: {
+                files: ['css/style.styl',
+                        'index.jade'
+                        ],
+
+
+                tasks: ['stylus','jade'],
+                options: {
+                    interrupt: true
+                }
+            }
+        },
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['uglify', 'jade', 'stylus', 'watch']);
+  grunt.registerTask('server', ['connect:server']);
+
+
+
+};
